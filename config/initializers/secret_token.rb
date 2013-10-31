2d4fc42f8e6b17cea6a432666a870bb4c591d150
Jao Zafra <jao@teamcodeflux.com>
2013-10-31 14:09:36 +0800

    Initial Commit. :D

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jikan::Application.config.secret_token = '84791028f4b8445057e4f27806f969992859205d89097347f7f691044baf6db5b76f85afafff844f59d6081f1c0a5ad742c8bcff068302d574359878191ccf2d'
