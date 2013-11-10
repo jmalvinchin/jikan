11366bd8375887aae10e47ded9ee8882a56b388f
Joseph Gabriel Zafra <joseph.zafra@gmail.com>
2013-11-10 16:49:42 +0800

    Added simple user access functionality.
    To access the application, you need to be signed in.

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  # attr_accessible :title, :body
+  attr_accessible :username, :email, :password, :password_confirmation, :remember_me
+  validates :username, presence: true
+end
